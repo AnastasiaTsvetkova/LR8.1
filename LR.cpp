ac2695a580fac1f9539ef722a676342f1f83a097
AnastasiaTsvetkova <86355641+AnastasiaTsvetkova@users.noreply.github.com>
2021-06-28 13:51:31 +0300

    Add files via upload

---
+++ LR.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i, x;
+    cin >> i;
+    while (i > 0) {
+        x = i % 10; //получаем остаток от деления на 10
+        i = i / 10; //делим на 10
+        cout << x << endl; 
+    }
+
+system("pause");
+return 0;
+
+}
